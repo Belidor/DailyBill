51bbefd0d9058c7dfe98962183534d0e142d7d2d
belidor <idietoday@ya.ru>
2023-04-30 10:35:37 +0200

    Initial Commit

---
+++ DailyBill/DailyBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyBillApp.swift
+//  DailyBill
+//
+//  Created by Артем Купрацевич on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
